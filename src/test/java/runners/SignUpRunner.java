96aac7235055e1101c362caad1a3c1ce642f637b
davidmoralv <davidmoralv>
2023-10-02 17:07:16 -0500

    Modify test runner

--- src/test/java/runners/SignUpRunner.java
+++ src/test/java/runners/SignUpRunner.java
@@ -1,15 +1,43 @@
 package runners;
 
 import io.cucumber.junit.CucumberOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
+import net.thucydides.core.environment.SystemEnvironmentVariables;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        features = "src/test/resources/features/A_SignUp.feature",
-        glue = "stepdefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
-        monochrome = true,
-        snippets = CucumberOptions.SnippetType.CAMELCASE)
+
 public class SignUpRunner {
+    @Before
+    public void test() {
+        switch (SystemEnvironmentVariables.createEnvironmentVariables().getProperty("webdriver.driver")) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                break;
+            case "ie":
+            case "iexplorer":
+                WebDriverManager.iedriver().setup();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                break;
+        }
+    }
+
+    @Test
+    public void testRunner() {
+        JUnitCore.runClasses(Runner.class);
+    }
+
+    @RunWith(CucumberWithSerenity.class)
+    @CucumberOptions(features = "src/test/resources/features/A_SignUp.feature",
+            glue = "stepdefinitions",
+            plugin = {"pretty", "html:target/cucumber-reports.html"},
+            monochrome = true,
+            snippets = CucumberOptions.SnippetType.CAMELCASE)
+    public class Runner {
+    }
 }
